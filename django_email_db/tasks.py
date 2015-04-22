2ea82181c3292667a41b933ddde34f11a329a21b
Ivan Metzlar <metzlar@gmail.com>
2015-04-22 14:36:04 -0700

    email message archiving

--- django_email_db/tasks.py
+++ django_email_db/tasks.py
@@ -1,8 +1,12 @@
 from __future__ import absolute_import
 
 from django.core.mail import get_connection
+from django.utils.timezone import now
+
+import datetime
 
 from .management.commands.send_messages import get_messages
+from .models import Message
 
 
 try:
@@ -12,6 +16,14 @@ try:
     @shared_task
     def send_email_batch():
         get_connection().send_messages_smtp(get_messages())
+
+
+    @shared_task
+    def archive_messages():
+        six_months_ago = now() - datetime.timedelta(days=180)
+        Message.objects.select_related('attachments').filter(
+            created__lte = six_months_ago
+        ).delete()
 
 
 except ImportError:
